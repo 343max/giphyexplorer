d267ca094cad564be9c158616f439b94f37b7951
Max von Webel <max@343max.de>
2018-07-31 12:36:29 -0700

    MediaRepresantion Model

---
+++ GiphyExplorer/Model/MediaRepresentation.swift
@@ -0,0 +1,54 @@
+// Copyright Max von Webel. All Rights Reserved.
+
+import Foundation
+
+struct MediaRepresentation : Decodable {
+    struct Size {
+        let width: Int
+        let height: Int
+    }
+    
+    struct Format {
+        let url: URL
+        let size: Int
+        
+        init?(url: URL?, size: Int?) {
+            if let url = url, let size = size {
+                self.url = url
+                self.size = size
+            } else {
+                return nil
+            }
+        }
+    }
+
+    let dimensions: Size
+    let mp4: Format?
+    
+    enum CodingKeys: String, CodingKey {
+        case width = "width"
+        case height = "height"
+        case mp4url = "mp4"
+        case mp4size = "mp4_size"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try! decoder.container(keyedBy: CodingKeys.self)
+        self.dimensions = Size(width: Int(try! container.decode(String.self, forKey: .width))!,
+                               height: Int(try! container.decode(String.self, forKey: .height))!)
+        
+        do {
+            let url = try? container.decode(URL.self, forKey: .mp4url)
+            let size = Int(try container.decode(String.self, forKey: .mp4size))
+            self.mp4 = Format(url: url, size: size)
+        } catch _ {
+            self.mp4 = nil
+        }
+    }
+}
+
+extension MediaRepresentation {
+    static func dict(data: Data) {
+        
+    }
+}
---
+++ GiphyExplorerTests/Model/MediaRepresentationTests.swift
@@ -0,0 +1,39 @@
+// Copyright Max von Webel. All Rights Reserved.
+
+import XCTest
+
+class MediaRepresentationTests: XCTestCase {
+
+    func testMediaRepresentation() {
+        let data = """
+{
+    "url": "https://media0.giphy.com/media/jxcilyPDPuublRty5I/100w.gif",
+    "width": "100",
+    "height": "57",
+    "size": "90555",
+    "mp4": "https://media0.giphy.com/media/jxcilyPDPuublRty5I/100w.mp4",
+    "mp4_size": "17429",
+    "webp": "https://media0.giphy.com/media/jxcilyPDPuublRty5I/100w.webp",
+    "webp_size": "96022"
+}
+""".data(using: .utf8)!
+        let mediaRepresentation = try! JSONDecoder().decode(Image.self, from: data)
+        XCTAssertEqual(mediaRepresentation.dimensions.width, 100)
+        XCTAssertEqual(mediaRepresentation.dimensions.height, 57)
+        XCTAssertEqual(mediaRepresentation.mp4!.url, URL(string: "https://media0.giphy.com/media/jxcilyPDPuublRty5I/100w.mp4"))
+        XCTAssertEqual(mediaRepresentation.mp4!.size, 17429)
+    }
+    
+    func testNoMp4() {
+        let data = """
+{
+    "url": "https://media0.giphy.com/media/jxcilyPDPuublRty5I/giphy-preview.gif",
+    "width": "149",
+    "height": "84",
+    "size": "49824"
+}
+""".data(using: .utf8)!
+        let mediaRepresentation = try! JSONDecoder().decode(Image.self, from: data)
+        XCTAssertNil(mediaRepresentation.mp4)
+    }
+}
