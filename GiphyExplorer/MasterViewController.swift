33a914a2015fb4db7413549ae6b51ffe578abde8
Max von Webel <max@343max.de>
2018-07-31 20:06:30 -0700

    loading & reloading kind of works

--- GiphyExplorer/MasterViewController.swift
+++ GiphyExplorer/MasterViewController.swift
@@ -1,12 +1,51 @@
 // Copyright Max von Webel. All Rights Reserved.
 
 import UIKit
+import AVKit
+
+class ImageCell: UICollectionViewCell {
+    weak var playerLayer: AVPlayerLayer?
+    var image: Image? {
+        didSet {
+            let notificationCenter = NotificationCenter.default
+            
+            notificationCenter.removeObserver(self, name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+            self.playerLayer?.removeFromSuperlayer()
+
+            guard let image = image else {
+                return
+            }
+            
+            let player = AVPlayer(url: image.mp4s["preview"]!.mp4!.url)
+            player.automaticallyWaitsToMinimizeStalling = true
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.frame = contentView.bounds
+            contentView.layer.addSublayer(playerLayer)
+            self.playerLayer = playerLayer
+            notificationCenter.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: OperationQueue.main) { _ in
+                player.seek(to: CMTime.zero)
+                player.play()
+            }
+            player.play()
+        }
+    }
+    
+    override func prepareForReuse() {
+        self.image = nil
+    }
+}
 
 class MasterViewController: UICollectionViewController {
-
     var detailViewController: DetailViewController? = nil
     var client: GiphyClient!
-    var images: [Image] = []
+    var images: [Image] = [] {
+        didSet {
+            self.collectionView?.reloadData()
+            refreshControl.endRefreshing()
+        }
+    }
+    
+    var refreshControl: UIRefreshControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +54,10 @@ class MasterViewController: UICollectionViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        
+        refreshControl = UIRefreshControl(frame: collectionView.bounds)
+        refreshControl.addTarget(self, action: #selector(reload), for: .valueChanged)
+        collectionView.addSubview(refreshControl)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -22,12 +65,27 @@ class MasterViewController: UICollectionViewController {
         super.viewWillAppear(animated)
         
         if images.count == 0 {
-            client.trending(offset: 0).then { [weak self] in
-                guard let self = self else {
-                    return
-                }
-                
-                self.images += $0.payload
+            loadMore(reload: true)
+        }
+    }
+    
+    @objc func reload() {
+        loadMore(reload: true)
+    }
+    
+    func loadMore(reload: Bool) {
+        let offset = reload ? 0 : images.count
+        
+        refreshControl.beginRefreshing()
+        client.trending(offset: offset).then { [weak self] (response) in
+            guard let self = self else {
+                return
+            }
+            
+            if response.page.offset < self.images.count {
+                self.images = response.payload
+            } else {
+                self.images += response.payload
             }
         }
     }
@@ -44,6 +102,22 @@ class MasterViewController: UICollectionViewController {
 //                controller.navigationItem.leftItemsSupplementBackButton = true
             }
         }
+    }
+    
+    // MARK: - CollectionView
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
+        cell.image = images[indexPath.item]
+        return cell
     }
 }
 
